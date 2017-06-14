659a58bc1741834c9dbb288d524b1a50272b2ab3
shubhamkr01 <skshubham2014@gmail.com>
2017-06-14 18:15:01 +0530

    Create open_schol_link.py

--- open_schol_link.py
+++ open_schol_link.py
@@ -1,1 +1,9 @@
 
+from selenium import webdriver
+import time
+
+def open_schol_link():
+	browser=webdriver.Chrome()
+	for i in range(len(urls)):
+		if(i!=0  and urls[i]!=''):
+			browser.get(urls[i])
