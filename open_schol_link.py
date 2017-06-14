09f97a1e5370783a11d9ead1ba008912813abe80
shubhamkr01 <skshubham2014@gmail.com>
2017-06-14 20:23:10 +0530

    Create open_schol_link.py

--- open_schol_link.py
+++ open_schol_link.py
@@ -2,8 +2,16 @@
 from selenium import webdriver
 import time
 
+redirect = []
+
 def open_schol_link():
 	browser=webdriver.Chrome()
 	for i in range(len(urls)):
 		if(i!=0  and urls[i]!=''):
 			browser.get(urls[i])
+			
+			#appending '1' for the redirecting urls to a list called redirect & '0' otherwise
+			if(browser.current_url!=urls[i]):
+				redirect.append('1')
+			else:
+				redirect.append('0')
