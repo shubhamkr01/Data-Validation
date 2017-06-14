d35928b0c78493ca36f60bf74a9ef4fb941c592e
shubhamkr01 <skshubham2014@gmail.com>
2017-06-14 15:42:52 +0530

    Create extract.py

--- extract.py
+++ extract.py
@@ -1,1 +1,14 @@
+import csv
 
+import xlrd
+
+workbook = xlrd.open_workbook('filePath')
+sheet=workbook.sheet_by_index(0)
+urls=[]
+for rownum in range(sheet.nrows):
+	urls.append(sheet.cell_value(rownum,18))
+  
+with open('E:/Shubhams_Library/validating/schol_url.csv','w') as writefile: #creates csv file
+	writer=csv.writer(writefile,delimiter="\n") #writes each list element into new column
+	writer.writerow(urls)
+  
