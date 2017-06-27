17dda5dd8345b6d80d5757046d58c0ecc7bcc97e
shubhamkr01 <skshubham2014@gmail.com>
2017-06-27 15:21:53 +0530

    Update urlvalidator.py

--- urlvalidator.py
+++ urlvalidator.py
@@ -1,7 +1,6 @@
 import urllib.request
 import urllib.parse
 newurl='http://jvsbvirbvievb.com'
-TypeError: 'module' object is not callable
 req=urllib.request.Request(newurl) 
 try:
 	response=urllib.request.urlopen(req)
