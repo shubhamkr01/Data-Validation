84f6fca1078ef8e2c93edad662015d4f0456b9d5
shubhamkr01 <skshubham2014@gmail.com>
2017-06-29 14:32:26 +0530

    Update extract.py

--- extract.py
+++ extract.py
@@ -6,11 +6,12 @@ sheet=workbook.sheet_by_index(0)
 
 urls=[]
 amount=[]
+name=[]
 
 for rownum in range(sheet.nrows):
 	urls.append(sheet.cell_value(rownum,18))
 	amount.append(sheet.cell_value(rownum,5))
-
+	name.append(sheet.cell_value(rownum,1))
   
 with open('E:/Shubhams_Library/validating/schol_url.csv','w') as writefile: #creates csv file
 	writer=csv.writer(writefile,delimiter="\n") #writes each list element into new column
