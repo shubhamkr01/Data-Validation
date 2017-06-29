4dfa5c9e8a364f5b4078308297385b0bb0836f2e
shubhamkr01 <skshubham2014@gmail.com>
2017-06-29 19:15:25 +0530

    Update get_target_name.py

--- get_target_name.py
+++ get_target_name.py
@@ -6,7 +6,7 @@ r=requests.get('http://www.uiw.edu/internationaladmissions/scholarships.html')
 
 
 f='WeberEdge International Scholarship'
-pattern=re.compile('(([A-Z]\w+\s)+Scholarship|([A-Z]\w+\s)+Award)')
+pattern=re.compile('(([A-Z]\w+\s)+Scholarship|([A-Z]\w+\s)+Award|([A-Z]\w+\s)+Waiver|([A-Z]\w+\s)+Fund|([A-Z]\w+\s)+Aid|([A-Z]\w+\s)+Grant|([A-Z]\w+\s)+Fellowship|([A-Z]\w+\s)+Assistantship'))
 r=requests.get('http://www.weber.edu/issc/top/scholarship.html')
 '''p=re.findall(pattern,r.text)
 for i in p:
