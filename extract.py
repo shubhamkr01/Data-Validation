5c607b8c16a66e34197a7b0b71b995cad642a209
shubhamkr01 <skshubham2014@gmail.com>
2017-06-29 14:57:38 +0530

    Update extract.py

--- extract.py
+++ extract.py
@@ -12,6 +12,8 @@ for rownum in range(sheet.nrows):
 	urls.append(sheet.cell_value(rownum,18))
 	amount.append(sheet.cell_value(rownum,5))
 	name.append(sheet.cell_value(rownum,1))
+
+# column names: redirect, invalid url, name mismatch, amount mismatch, amount found
   
 with open('E:/Shubhams_Library/validating/schol_url.csv','w') as writefile: #creates csv file
 	writer=csv.writer(writefile,delimiter="\n") #writes each list element into new column
