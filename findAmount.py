d19d8466b47110760818934b2798f9f27c2ac660
shubhamkr01 <skshubham2014@gmail.com>
2017-06-30 20:46:48 +0530

    Update findAmount.py

--- findAmount.py
+++ findAmount.py
@@ -4,7 +4,7 @@ import requests
 
 r=requests.get('http://www.uiw.edu/internationaladmissions/scholarships.html')
 
-signpattern=re.compile('[$]\d{1,1000},*\d{1,1000}')
+signpattern=re.compile('([$]?((\d{1,1000},)*\d{1,1000}(\b)(-)*[$]?((\d{1,1000},)*\d{1,1000})))')
 amount=re.findall(signpattern,r.text)
 
 #amount = ['$8,000']
